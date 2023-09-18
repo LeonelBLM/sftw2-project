322d140e656905c502a5601e8fa234d3cc9473c9
Flavia Moscoso <flavia.celeste1999@gmail.com>
2023-09-18 11:15:34 -0400

    initial commit create next-app

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth/next';
+import GithubProvider from 'next-auth/providers/github'
+//import { MongoDBAdapter } from '@auth/mongodb-adapter'
+//import clientPromise from 'lib/mongodb'
+export default NextAuth({
+     //adapter: MongoDBAdapter(clientPromise),
+    providers: [
+            GithubProvider({
+                clientId: process.env.GITHUB_ID,
+                clientSecret: process.env.GITHUB_SECRET,
+            })
+    ]
+})
